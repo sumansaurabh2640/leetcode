33a27258817757db2fac5424f2652c0887df3f9b
SUMAN SAURABH <sumansaurabhss2640@gmail.com>
2026-08-12 22:36:10 +0530

    Time: 132 ms (66.86%), Space: 112.2 MB (64.96%) - LeetHub

---
+++ 0220-contains-duplicate-iii/0220-contains-duplicate-iii.java
@@ -0,0 +1,29 @@
+import java.util.TreeSet;
+
+class Solution {
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int indexDiff, int valueDiff) {
+        TreeSet<Long> set = new TreeSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            long num = nums[i];
+
+            // Find the smallest number >= num - valueDiff
+            Long candidate = set.ceiling(num - (long) valueDiff);
+
+            // Check if candidate is within valueDiff
+            if (candidate != null && candidate <= num + (long) valueDiff) {
+                return true;
+            }
+
+            set.add(num);
+
+            // Keep only the previous indexDiff elements
+            if (i >= indexDiff) {
+                set.remove((long) nums[i - indexDiff]);
+            }
+        }
+
+        return false;
+    }
+}
