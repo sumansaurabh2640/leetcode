ff16458cc4531338956759bee3b5b8af4786292c
SUMAN SAURABH <sumansaurabhss2640@gmail.com>
2026-08-12 22:41:43 +0530

    Time: 0 ms (100%), Space: 45.4 MB (79%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,36 +1,75 @@
+// import java.util.*;
+
+// class Solution {
+//     public List<List<Integer>> permute(int[] nums) {
+//         List<List<Integer>> result = new ArrayList<>();
+//         boolean[] used = new boolean[nums.length];
+
+//         backtrack(nums, used, new ArrayList<>(), result);
+
+//         return result;
+//     }
+
+//     private void backtrack(int[] nums, boolean[] used,
+//                            List<Integer> current,
+//                            List<List<Integer>> result) {
+
+//         if (current.size() == nums.length) {
+//             result.add(new ArrayList<>(current));
+//             return;
+//         }
+
+//         for (int i = 0; i < nums.length; i++) {
+//             if (used[i]) {
+//                 continue;
+//             }
+
+//             used[i] = true;
+//             current.add(nums[i]);
+
+//             backtrack(nums, used, current, result);
+
+//             current.remove(current.size() - 1);
+//             used[i] = false;
+//         }
+//     }
+// }
 import java.util.*;
 
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        boolean[] used = new boolean[nums.length];
 
-        backtrack(nums, used, new ArrayList<>(), result);
+        backtrack(nums, 0, result);
 
         return result;
     }
 
-    private void backtrack(int[] nums, boolean[] used,
-                           List<Integer> current,
-                           List<List<Integer>> result) {
+    private void backtrack(int[] nums, int start, List<List<Integer>> result) {
+        if (start == nums.length) {
+            List<Integer> list = new ArrayList<>();
 
-        if (current.size() == nums.length) {
-            result.add(new ArrayList<>(current));
+            for (int num : nums) {
+                list.add(num);
+            }
+
+            result.add(list);
             return;
         }
 
-        for (int i = 0; i < nums.length; i++) {
-            if (used[i]) {
-                continue;
-            }
+        for (int i = start; i < nums.length; i++) {
+            // Swap
+            int temp = nums[start];
+            nums[start] = nums[i];
+            nums[i] = temp;
 
-            used[i] = true;
-            current.add(nums[i]);
+            // Generate remaining permutations
+            backtrack(nums, start + 1, result);
 
-            backtrack(nums, used, current, result);
-
-            current.remove(current.size() - 1);
-            used[i] = false;
+            // Backtrack: undo swap
+            temp = nums[start];
+            nums[start] = nums[i];
+            nums[i] = temp;
         }
     }
 }
