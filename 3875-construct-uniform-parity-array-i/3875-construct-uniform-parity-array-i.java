c6b06f3aa3a9d14f3e3c553fa9414618a02c6cd7
SUMAN SAURABH <sumansaurabhss2640@gmail.com>
2026-09-02 14:36:15 +0530

    Time: 0 ms (100%), Space: 45.2 MB (57.87%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -0,0 +1,11 @@
+// class Solution {
+//     public boolean uniformArray(int[] nums1) {
+        
+//     }
+// }
+
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        return true;
+    }
+}
