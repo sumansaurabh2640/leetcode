fe045741fb4a3278058347e8412d383ae4ef89bd
SUMAN SAURABH <sumansaurabhss2640@gmail.com>
2026-09-04 19:17:09 +0530

    Time: 4 ms (99.56%), Space: 53.7 MB (41.89%) - LeetHub

---
+++ 0089-gray-code/0089-gray-code.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> grayCode(int n) {
+        List<Integer> ans = new ArrayList<>();
+
+        int size = 1 << n;
+
+        for (int i = 0; i < size; i++) {
+            ans.add(i ^ (i >> 1));
+        }
+
+        return ans;
+    }
+}
