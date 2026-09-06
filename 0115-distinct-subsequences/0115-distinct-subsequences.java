ac5ddd4acd13ca02ab2322364d3e543e15cbc1a7
SUMAN SAURABH <sumansaurabhss2640@gmail.com>
2026-09-06 16:17:29 +0530

    Time: 14 ms (92.72%), Space: 43 MB (88.4%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int numDistinct(String s, String t) {
+        int m = s.length();
+        int n = t.length();
+
+        int[] dp = new int[n + 1];
+
+        // Empty t can be formed in exactly one way
+        dp[0] = 1;
+
+        for (int i = 1; i <= m; i++) {
+
+            // Go backwards so that dp[j - 1]
+            // still represents the previous row
+            for (int j = n; j >= 1; j--) {
+
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[j] += dp[j - 1];
+                }
+            }
+        }
+
+        return dp[n];
+    }
+}
