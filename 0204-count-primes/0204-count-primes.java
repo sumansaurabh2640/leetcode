31245d3a950986072e236bf18753720efd060228
SUMAN SAURABH <sumansaurabhss2640@gmail.com>
2026-09-08 23:33:22 +0530

    Time: 786 ms (12.1%), Space: 80.1 MB (5.46%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,3 +1,40 @@
+// class Solution {
+//     public int countPrimes(int n) {
+//         if (n <= 2) {
+//             return 0;
+//         }
+
+//         boolean[] isPrime = new boolean[n];
+
+//         // Assume all numbers are prime
+//         for (int i = 2; i < n; i++) {
+//             isPrime[i] = true;
+//         }
+
+//         // Sieve of Eratosthenes
+//         for (int i = 2; i * i < n; i++) {
+
+//             if (isPrime[i]) {
+
+//                 // Start from i * i
+//                 for (int j = i * i; j < n; j += i) {
+//                     isPrime[j] = false;
+//                 }
+//             }
+//         }
+
+//         int count = 0;
+
+//         for (int i = 2; i < n; i++) {
+//             if (isPrime[i]) {
+//                 count++;
+//             }
+//         }
+
+//         return count;
+//     }
+// }
+
 class Solution {
     public int countPrimes(int n) {
         if (n <= 2) {
@@ -6,17 +43,12 @@ class Solution {
 
         boolean[] isPrime = new boolean[n];
 
-        // Assume all numbers are prime
         for (int i = 2; i < n; i++) {
             isPrime[i] = true;
         }
 
-        // Sieve of Eratosthenes
         for (int i = 2; i * i < n; i++) {
-
             if (isPrime[i]) {
-
-                // Start from i * i
                 for (int j = i * i; j < n; j += i) {
                     isPrime[j] = false;
                 }
