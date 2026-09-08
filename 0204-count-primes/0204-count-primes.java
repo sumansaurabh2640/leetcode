daf2ac7d9b190c1d2a4b75b391b23fcf2b5b4f53
SUMAN SAURABH <sumansaurabhss2640@gmail.com>
2026-09-08 23:28:55 +0530

    Time: 795 ms (7.67%), Space: 79.9 MB (16.45%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int countPrimes(int n) {
+        if (n <= 2) {
+            return 0;
+        }
+
+        boolean[] isPrime = new boolean[n];
+
+        // Assume all numbers are prime
+        for (int i = 2; i < n; i++) {
+            isPrime[i] = true;
+        }
+
+        // Sieve of Eratosthenes
+        for (int i = 2; i * i < n; i++) {
+
+            if (isPrime[i]) {
+
+                // Start from i * i
+                for (int j = i * i; j < n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        int count = 0;
+
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i]) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
